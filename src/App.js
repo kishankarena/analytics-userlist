731db387309ce95a8e6dc63b8e8941e0ee0a6c30
kishankarena <kishan.k@simformsolutions.com>
2024-06-05 17:36:51 +0530

    feature : integrate pageview analytics

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
 import "./App.css";
 import UserComponentWrapper from "./components/componentWrapper/UserComponentWrapper";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-KY2K2WN95M");
 
 function App() {
+  useEffect(() => {
+    ReactGA.send({
+      hitType: "pageview",
+      page: window.location.pathname + window.location.search,
+    });
+  }, []);
   return (
     <div>
       <UserComponentWrapper />
